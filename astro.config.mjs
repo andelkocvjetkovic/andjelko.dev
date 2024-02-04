9536b45fc26f9eb23f57f3abf0ddf4aa7a600f1b
Andjelko Cvjetkovic <andjelko.cvjetkovic@gmail.com>
2024-02-04 21:02:09 +0100

    add dark mode and blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 import { defineConfig,passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: passthroughImageService(),
   },
-   integrations: [
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'solarized-dark',
+    },
+  },
+  integrations: [
      react(),
-     tailwind()
+     tailwind(),
+     mdx(),
    ],
 });
