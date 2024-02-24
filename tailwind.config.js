4cdbfb42b2865ad8711125ab3440a1a12006d125
Andjelko Cvjetkovic <andjelko.cvjetkovic@gmail.com>
2024-02-24 20:36:38 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,20 @@ content: ['./src/**/*.{astro,html,md,mdx,ts,tsx}'],
       container: {
         center: true,
         padding: "1rem",
+        screens: {
+          sm: '100%',
+          md: '65ch',
+          lg: '65ch',
+          xl: '70ch',
+          '2xl': '80ch',
+        },
       },
       fontFamily: {
-        sans: ["Be Vietnam Pro", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter Variable', 'sans-serif'],
       },
+      colors: {
+        primary: '#FAE500'
+      }
     },
   },
   plugins: [
